b97e7dd31419d9f45b2901c5d20267c3bd2b0675
zxfjd3g <258147149@qq.com>
2018-04-29 16:47:44 +0800

    更新用户和获取用户

--- routes/index.js
+++ routes/index.js
@@ -81,6 +81,46 @@ router.post('/login', function (req, res) {
   })
 })
 
+// 更新用户信息的路由
+router.post('/update', function (req, res) {
+  // 从请求的cookie得到userid
+  const userid = req.cookies.userid
+  // 如果不存在, 直接返回一个提示信息
+  if(!userid) {
+    return res.send({code: 1, msg: '请先登陆'})
+  }
+  // 存在, 根据userid更新对应的user文档数据
+  // 得到提交的用户数据
+  const user = req.body // 没有_id
+  UserModel.findByIdAndUpdate({_id: userid}, user, function (error, oldUser) {
 
+    if(!oldUser) {
+      // 通知浏览器删除userid cookie
+      res.clearCookie('userid')
+      // 返回返回一个提示信息
+      res.send({code: 1, msg: '请先登陆'})
+    } else {
+      // 准备一个返回的user数据对象
+      const {_id, username, type} = oldUser
+      const data = Object.assign({_id, username, type}, user)
+      // 返回
+      res.send({code: 0, data})
+    }
+  })
+})
+
+// 获取用户信息的路由(根据cookie中的userid)
+router.get('/user', function (req, res) {
+  // 从请求的cookie得到userid
+  const userid = req.cookies.userid
+  // 如果不存在, 直接返回一个提示信息
+  if(!userid) {
+    return res.send({code: 1, msg: '请先登陆'})
+  }
+  // 根据userid查询对应的user
+  UserModel.findOne({_id: userid}, filter, function (error, user) {
+    res.send({code: 0, data: user})
+  })
+})
 
 module.exports = router;

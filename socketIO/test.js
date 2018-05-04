43cd0a34ca4a0b746c5477b43e58d9c2942da3ba
zxfjd3g <258147149@qq.com>
2018-05-04 09:45:05 +0800

    socketio

--- socketIO/test.js
+++ socketIO/test.js
@@ -1,16 +1,19 @@
 module.exports = function (server) {
-  // 得到IO对象
   const io = require('socket.io')(server)
-  // 监视连接(当有一个客户连接上时回调)
+
+  // 监视客户端与服务器的连接
   io.on('connection', function (socket) {
-    console.log('soketio connected')
-    // 绑定sendMsg监听, 接收客户端发送的消息
+    console.log('有一个客户端连接上了服务器')
+
+    // 绑定监听, 接收客户端发送的消息
     socket.on('sendMsg', function (data) {
-      console.log('服务器接收到浏览器的消息', data)
-      // 向客户端发送消息(名称, 数据)
-      io.emit('receiveMsg', data.name + '_' + data.date)  // 发送所有连接上服务器的客户端
-      // socket.emit('receiveMsg', data.name + '_' + data.date) // 发送给当前socket对应的客户端
-      console.log('服务器向浏览器发送消息',  data.name + '_' + data.date)
+      console.log('服务器接收到客户端发送的消息', data)
+      // 处理数据
+      data.name = data.name.toUpperCase()
+      // 服务器向客户端发送消息
+      // socket.emit('receiveMsg', data)
+      io.emit('receiveMsg', data)
+      console.log('服务器向客户端发送消息', data)
     })
   })
 }
